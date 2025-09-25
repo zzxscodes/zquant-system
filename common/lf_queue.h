c4a817e4454c7a00025943edad1eb9df33f111a8
zzxscodes <129288947+zzxscodes@users.noreply.github.com>
2025-09-25 16:04:09 +0800

    Update lf_queue.h

--- common/lf_queue.h
+++ common/lf_queue.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <atomic>
+#include <immintrin.h>
 
 #include "macros.h"
 
@@ -12,29 +13,52 @@ namespace Common {
   public:
     explicit LFQueue(std::size_t num_elems) :
         store_(round_up_to_power_of_2(num_elems), T()),
-        mask_(store_.size() - 1) {
+        mask_(store_.size() - 1),
+        capacity_(store_.size()) {
     }
 
-    auto getNextToWriteTo() noexcept {
-      return &store_[next_write_index_.load(std::memory_order_relaxed) & mask_];
+    auto tryGetNextToWriteTo() noexcept -> T* {
+      auto current_write = next_write_index_.load(std::memory_order_relaxed);
+      auto current_read = next_read_index_.load(std::memory_order_acquire);
+      
+      if (UNLIKELY((current_write + 1) & mask_ == current_read & mask_)) {
+        return nullptr;
+      }
+      return &store_[current_write & mask_];
+    }
+
+    auto getNextToWriteTo() noexcept -> T* {
+      while (true) {
+        auto slot = tryGetNextToWriteTo();
+        if (LIKELY(slot != nullptr)) {
+          return slot;
+        }
+        
+        _mm_pause();
+      }
     }
 
     auto updateWriteIndex() noexcept {
       auto current_write_index = next_write_index_.load(std::memory_order_relaxed);
-      next_write_index_.store(current_write_index + 1, std::memory_order_relaxed);
-      ASSERT(num_elements_ != 0, "Read an invalid element in:" + std::to_string(pthread_self()));
+      next_write_index_.store(current_write_index + 1, std::memory_order_release);
       num_elements_.fetch_add(1, std::memory_order_release);
     }
 
     auto getNextToRead() const noexcept -> const T * {
-      auto current_read_index = next_read_index_.load(std::memory_order_relaxed);
-      return (num_elements_.load(std::memory_order_acquire) ? 
-              &store_[current_read_index & mask_] : nullptr);
+        auto current_read_index = next_read_index_.load(std::memory_order_relaxed);
+        auto current_element_count = num_elements_.load(std::memory_order_acquire);
+    
+        if (LIKELY(current_element_count > 0)) {
+            std::size_t target_index = current_read_index & mask_;
+            return &store_[target_index];
+        } else {
+            return nullptr;
+        }
     }
 
     auto updateReadIndex() noexcept {
       auto current_read_index = next_read_index_.load(std::memory_order_relaxed);
-      next_read_index_.store(current_read_index + 1, std::memory_order_relaxed);
+      next_read_index_.store(current_read_index + 1, std::memory_order_release);
       num_elements_.fetch_sub(1, std::memory_order_release);
     }
 
@@ -42,7 +66,16 @@ namespace Common {
       return num_elements_.load(std::memory_order_acquire);
     }
 
-    /// Deleted default, copy & move constructors and assignment-operators.
+    auto is_full() const noexcept -> bool {
+      auto current_write = next_write_index_.load(std::memory_order_relaxed);
+      auto current_read = next_read_index_.load(std::memory_order_relaxed);
+      return (current_write + 1) & mask_ == current_read & mask_;
+    }
+
+    auto capacity() const noexcept -> std::size_t {
+      return capacity_;
+    }
+
     LFQueue() = delete;
     LFQueue(const LFQueue &) = delete;
     LFQueue(const LFQueue &&) = delete;
@@ -51,7 +84,7 @@ namespace Common {
 
   private:
     static std::size_t round_up_to_power_of_2(std::size_t v) {
-      if (v == 0) return 1;
+      if (UNLIKELY(v == 0)) return 1;
       
       --v;
       v |= v >> 1;
@@ -65,14 +98,12 @@ namespace Common {
       return v;
     }
 
-    /// Underlying container of data accessed in FIFO order.
     std::vector<T> store_;
     const std::size_t mask_;
+    const std::size_t capacity_;
 
-    /// Atomic trackers for next index to write new data to and read new data from.
     alignas(64) std::atomic<std::size_t> next_write_index_ = {0};
     alignas(64) std::atomic<std::size_t> next_read_index_ = {0};
-
     alignas(64) std::atomic<std::size_t> num_elements_ = {0};
   };
 }
